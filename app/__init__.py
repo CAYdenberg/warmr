b3c0ed648adca09c9f7933be32abbbfc6656e876
@CAYdenberg <ydenberg@gmail.com>
2018-09-20 10:19:42 -0600

    Make config readable

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,10 @@
 from flask import Flask, Response
 from flask_pymongo import PyMongo
-from dotenv import load_dotenv
-import os
-
-# load dotenv in the base root
-APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
-dotenv_path = os.path.join(APP_ROOT, '.env')
-load_dotenv(dotenv_path)
+from config import Config
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+app.config.from_object(Config)
+
 db = PyMongo(app)
 
 from app import views
