68a40999de2940357a6468192ee546de403aa98e
@CAYdenberg <ydenberg@gmail.com>
2018-10-23 21:16:20 -0600

    Add temperature gradient

--- src/Warming.js
+++ src/Warming.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {XYPlot, XAxis, YAxis, LineSeries} from 'react-vis'
+import {XYPlot, XAxis, YAxis, LineSeries, VerticalRectSeries, GradientDefs} from 'react-vis'
 
 import {
   lastValue,
@@ -32,6 +32,13 @@ const Warming = props => {
       height={600}
       xDomain={[1965, 2050]}
     >
+      <GradientDefs>
+        <linearGradient id="warming-range" x0="0" x2="0" y1="0" y2="1">
+          <stop offset="0%" stopColor="red" stopOpacity={0} />
+          <stop offset="50%" stopColor="red" stopOpacity={0.5} />
+          <stop offset="100%" stopColor="red" stopOpacity={0} />
+        </linearGradient>
+      </GradientDefs>
 
       <XAxis
         tickValues={[1965, 1985, 2005, 2025, 2045]}
@@ -43,6 +50,14 @@ const Warming = props => {
         position="middle"
         width={50}
         tickFormat={x => x / 1000}
+      />
+
+      <YAxis
+        position="middle"
+        orientation="right"
+        left={550}
+        width={50}
+        tickFormat={x => x / (2.5 * 10e5)}
       />
 
       {/*
@@ -66,6 +81,17 @@ const Warming = props => {
         stroke={COLOR}
         className="line-series"
         strokeStyle="dashed"
+      />
+
+      <VerticalRectSeries
+        data={cumulativeProjectedTotals.map((value, i) => {
+          const base = value + lastValue(cumulativeWorldTotals)
+          return {x: i + 2019, x0: i + 2018, y: base * 1.5, y0: base * 0.5}
+        })}
+        fill="url(#warming-range)"
+        className="line-series"
+        stroke="none"
+        cluster="a"
       />
 
     </XYPlot>
